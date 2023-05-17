f35b5ac3e4ecdb35625a6e1d755c1727723ce5a1
kldayrit <101092279+kldayrit@users.noreply.github.com>
2023-05-17 18:32:00 +0800

    Update InGameChat.java
    refactor

--- project/src/InGameChat.java
+++ project/src/InGameChat.java
@@ -1,6 +1,7 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import javax.swing.*;
 // source : https://www.geeksforgeeks.org/a-group-chat-application-in-java/
 public class InGameChat {
     private static final String TERMINATE = "Exit";
@@ -26,8 +27,7 @@ public class InGameChat {
                 //this on localhost only (For a subnet set it as 1)
                   
                 socket.joinGroup(group);
-                Thread t = new Thread(new
-                ReadThread(socket,group,port));
+                Thread t = new Thread(new ReadThread(socket,group,port));
               
                 // Spawn a thread for reading messages
                 t.start(); 
@@ -61,6 +61,44 @@ public class InGameChat {
             {
                 System.out.println("Error reading/writing from/to socket");
                 ie.printStackTrace();
+            }
+        }
+    }
+    
+}
+
+class ReadThread implements Runnable {
+    private MulticastSocket socket;
+    private InetAddress group;
+    private int port;
+    private static final int MAX_LEN = 1000;
+    ReadThread(MulticastSocket socket,InetAddress group,int port)
+    {
+        this.socket = socket;
+        this.group = group;
+        this.port = port;
+    }
+      
+    @Override
+    public void run()
+    {
+        while(!InGameChat.finished)
+        {
+                byte[] buffer = new byte[ReadThread.MAX_LEN];
+                DatagramPacket datagram = new
+                DatagramPacket(buffer,buffer.length,group,port);
+                String message;
+            try
+            {
+                socket.receive(datagram);
+                message = new
+                String(buffer,0,datagram.getLength(),"UTF-8");
+                if(!message.startsWith(InGameChat.name))
+                    System.out.println(message);
+            }
+            catch(IOException e)
+            {
+                System.out.println("Socket closed!");
             }
         }
     }
